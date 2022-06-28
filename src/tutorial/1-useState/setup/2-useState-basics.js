165c75b678e3d67aca741eaa076fe746a23ac038
Harshvardhan Joshi <48888309+harshjoshi23@users.noreply.github.com>
2022-06-29 01:57:07 +0530

    Adding the src

---
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+//Named import so curly braces
+
+const UseStateBasics = () => {
+  return <h2>useState basic example</h2>;
+};
+
+export default UseStateBasics;
