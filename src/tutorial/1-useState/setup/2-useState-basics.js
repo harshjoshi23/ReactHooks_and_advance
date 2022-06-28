836048f24f554cdc6edbf0de02d03610663d162c
Harshvardhan Joshi <48888309+harshjoshi23@users.noreply.github.com>
2022-06-29 02:29:54 +0530

    DynamicTitle

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,8 +1,24 @@
 import React, { useState } from "react";
-//Named import so curly braces
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, SetText] = useState("random title");
+
+  const handleClick = () => {
+    if (text === "random title") {
+      SetText("hello world");
+    } else {
+      SetText("random title");
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h1>{text} </h1>
+      <button className="btn" onClick={handleClick}>
+        Change Tile
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
