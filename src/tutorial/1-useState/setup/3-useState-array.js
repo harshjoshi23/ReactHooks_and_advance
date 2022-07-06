a58645b9080c47026d786e82ba8151f3a9b5ee99
Harshvardhan Joshi <48888309+harshjoshi23@users.noreply.github.com>
2022-07-06 17:47:12 +0530

    remove indi elements

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,17 +3,27 @@ import { data } from "../../../data";
 
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
+  const removeItem = (id) => {
+    //runs only when we cliced on button
+    //now it only looks for the id and we have to return it
+    // can write functuionality in param setPeople()
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
   return (
     <>
       {
         //iterate over our array
         people.map((person) => {
           const { id, name } = person;
+
           return (
             <div key={id} className="item">
               <h4>
                 {id}, {name}{" "}
               </h4>
+              <button onClick={() => removeItem(id)}>Remove Item</button>
             </div>
           );
         })
