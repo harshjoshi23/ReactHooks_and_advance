4145ae735c53d0e0872067feffb28d33e0c89b3f
Harshvardhan Joshi <48888309+harshjoshi23@users.noreply.github.com>
2022-07-08 23:36:37 +0530

    setting up the obj and returnng

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "harsh",
+    age: 18,
+    message: "hello bro",
+  });
+
+  const changeMessage = () => {
+    //invoking set person
+    setPerson("HelloWorld");
+  };
+  return (
+    <>
+      <h3>{person.name} </h3>
+      <h3>{person.age} </h3>
+      <h3>{person.message} </h3>
+      <button className="btn" onClick={changeMessage}>
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
