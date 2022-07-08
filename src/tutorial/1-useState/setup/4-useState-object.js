bfa749ea8358a0da5bd9e22bcfcfd1844557bd3e
Harshvardhan Joshi <48888309+harshjoshi23@users.noreply.github.com>
2022-07-08 23:56:19 +0530

    usestatecounter

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,23 +1,28 @@
 import React, { useState } from "react";
 
 const UseStateObject = () => {
-  const [person, setPerson] = useState({
-    name: "harsh",
-    age: 18,
-    message: "hello bro",
-  });
+  const [person, setPerson] =
+    useState();
+    //   {
+    //   name: "harsh",
+    //   age: 18,
+    //   message: "hello bro",
+    // }
+
+  const [name, setName] = useState("harsh");
+  const [age, setAge] = useState("22");
+  const [message, setMessage] = useState("Wassup2");
 
   const changeMessage = () => {
-    //orignally we have an obj so we need to change
-    // the state properly
     //using a spread operator
-    setPerson({ ...person, message: "Wassup" });
+    // setPerson({ ...person, message: "Wassup1" });
+    setMessage("FinalDestination");
   };
   return (
     <>
-      <h3>{person.name} </h3>
-      <h3>{person.age} </h3>
-      <h3>{person.message} </h3>
+      <h3>{name} </h3>
+      <h3>{age} </h3>
+      <h3>{message} </h3>
       <button className="btn" onClick={changeMessage}>
         Change Message
       </button>
